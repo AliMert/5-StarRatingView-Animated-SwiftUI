3fcf778b32b2017a0540b8ecb63ed2c68a2ee39c
Ali Mert Özhayta <ozhayta@zoho.com>
2022-05-02 16:27:23 +0300

    Initial Commit

---
+++ RatingAnimation/RatingAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatingAnimationApp.swift
+//  RatingAnimation
+//
+//  Created by Ali Mert Özhayta on 2.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RatingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
